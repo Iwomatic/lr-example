b137de2cc410e462b13e39dce7b9c9aac58fdbb0
ibai.ruiz <ibai.ruiz@GICA-MAD-295.grupoica.local>
2017-05-13 12:19:12 +0200

    Se añade Maven, limpia el dockerfile y se añade maven al path

--- DockerImageSources/Ejercicio/scripts/run-exercise.sh
+++ DockerImageSources/Ejercicio/scripts/run-exercise.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo Lanzando Jenkins en el puerto 8081
-#java -jar /usr/share/jenkins/jenkins.war --httpPort=8081 >> /var/jenkins_home/jenkins_log.log
 nohup java -jar /usr/share/jenkins/jenkins.war --httpPort=8081 &
 echo Lanzando Liferay en el puerto 8080 y HSQL
-#./opt/liferay-portal-6.2-ce-ga6/tomcat-7.0.62/bin/startup.sh >> /var/liferay-home/lr_log.log
 nohup ./opt/liferay-portal-6.2-ce-ga6/tomcat-7.0.62/bin/startup.sh &
+MAVEN_HOME=/opt/apache-maven-3.5.0/bin
+export MAVEN_HOME=/opt/apache-maven-3.5.0
+PATH=$PATH:$MAVEN_HOME/bin
 echo "El ejemplo esta corriendo"
 bash
