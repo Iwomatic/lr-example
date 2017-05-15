145253a8254ced210e19b455ba265958f125b4a5
ibai.ruiz <ibai.ruiz@GICA-MAD-295.grupoica.local>
2017-05-15 11:32:08 +0200

    Fixes para incluir un primer checkout del código fuente y poderlo compilar

--- DockerImageSources/Ejercicio/scripts/run-exercise.sh
+++ DockerImageSources/Ejercicio/scripts/run-exercise.sh
@@ -6,5 +6,13 @@ nohup ./opt/liferay-portal-6.2-ce-ga6/tomcat-7.0.62/bin/startup.sh &
 MAVEN_HOME=/opt/apache-maven-3.5.0/bin
 export MAVEN_HOME=/opt/apache-maven-3.5.0
 PATH=$PATH:$MAVEN_HOME/bin
+echo "Inicializando codigo fuente en /var/setup/fuentes
+cd /var/setup/fuentes
+git init
+git config core.sparseCheckout true
+git remote add -f origin https://github.com/Iwomatic/lr-example.git
+echo "LiferayExercise/*" > .git/info/sparse-checkout
+git checkout master
+cd /var/setup/
 echo "El ejemplo esta corriendo"
 bash
